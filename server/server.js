c62aaa7ab76f7f3827fee70622c896c1cd546418
nishantAQ <nishant73883@gmail.com>
2025-04-29 23:08:48 +0530

    thisd commit

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', '']
+const allowedOrigins = ['https://green-cart-z4jl.vercel.app/', '']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
